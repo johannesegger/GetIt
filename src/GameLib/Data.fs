d42be4cfbaf878d90bbd7d390189fc8b2e2cdadc
Johannes Egger <j.egger@posteo.at>
2018-08-31 10:22:53 +0200

    Format code

--- src/GameLib/Data.fs
+++ src/GameLib/Data.fs
@@ -9,9 +9,9 @@ type Position =
     { X: float
       Y: float }
     static member (+) (p1: Position, p2: Position) =
-          { X = p1.X + p2.X; Y = p1.Y + p2.Y }
+        { X = p1.X + p2.X; Y = p1.Y + p2.Y }
     static member (-) (p1: Position, p2: Position) =
-          { X = p1.X - p2.X; Y = p1.Y - p2.Y }
+        { X = p1.X - p2.X; Y = p1.Y - p2.Y }
 
 type Size =
     { Width: float
