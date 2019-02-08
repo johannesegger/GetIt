49093175f1e0cbd2438176aa357ff284553ec3ff
Johannes Egger <j.egger@posteo.at>
2019-02-08 12:07:34 +0100

    Instantiate Fabulous template

---
+++ GetIt.WPF/App.fs
@@ -0,0 +1,21 @@
+﻿namespace GetIt.WPF
+
+open System
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.WPF
+
+type MainWindow() = 
+    inherit FormsApplicationPage()
+
+module Main = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main(_args) =
+
+        let app = new System.Windows.Application()
+        Forms.Init()
+        let window = MainWindow() 
+        window.LoadApplication(new GetIt.App())
+
+        app.Run(window)
