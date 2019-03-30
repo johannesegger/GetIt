b4eef1c855a8b315e2b8f84a9b8702075b8e1096
Johannes Egger <j.egger@posteo.at>
2019-03-30 16:22:41 +0100

    Format code

--- GetIt.Controller/Controller.fs
+++ GetIt.Controller/Controller.fs
@@ -221,46 +221,45 @@ module internal UICommunication =
 type Player(playerId) =
     let mutable isDisposed = 0
 
-    member internal x.PlayerId with get() = playerId
-    member private x.Player with get() = Map.find playerId (Model.getCurrent().Players)
+    member internal x.PlayerId with get () = playerId
+    member private x.Player with get () = Map.find playerId (Model.getCurrent().Players)
     /// <summary>
     /// The actual size of the player.
     /// </summary>
-    member x.Size with get() = x.Player.Size
+    member x.Size with get () = x.Player.Size
 
     /// <summary>
     /// The factor that is used to resize the player.
     /// </summary>
-    member x.SizeFactor with get() = x.Player.SizeFactor
+    member x.SizeFactor with get () = x.Player.SizeFactor
 
     /// <summary>
     /// The position of the player.
     /// </summary>
-    member x.Position with get() = x.Player.Position
+    member x.Position with get () = x.Player.Position
 
     /// <summary>
     /// The actual bounds of the player.
     /// </summary>
-    member x.Bounds with get() = x.Player.Bounds
+    member x.Bounds with get () = x.Player.Bounds
 
     /// <summary>
     /// The direction of the player.
     /// </summary>
-    member x.Direction with get() = x.Player.Direction
+    member x.Direction with get () = x.Player.Direction
 
     /// <summary>
     /// The pen of the player.
     /// </summary>
-    member x.Pen with get() = x.Player.Pen
+    member x.Pen with get () = x.Player.Pen
 
     abstract member Dispose: unit -> unit
-    default x.Dispose() =
-        if Interlocked.Exchange(&isDisposed, 1) = 0
-        then
+    default x.Dispose () =
+        if Interlocked.Exchange (&isDisposed, 1) = 0 then
             UICommunication.sendCommand (RemovePlayer playerId)
 
     interface IDisposable with
-        member x.Dispose() = x.Dispose()
+        member x.Dispose () = x.Dispose ()
 
 module internal Game =
     let mutable defaultTurtle = None
