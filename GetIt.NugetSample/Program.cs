e8382c942e0e37843825be363fa00f4964011221
Johannes Egger <j.egger@posteo.at>
2019-04-08 21:25:36 +0200

    Add sample that uses NuGet package

---
+++ GetIt.NugetSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GetIt;
+
+namespace GetIt.NugetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.ShowSceneAndAddTurtle();
+
+            Turtle.Say("Hello from NuGet!");
+        }
+    }
+}
