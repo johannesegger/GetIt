30ce16b2c0c1f9f76fd9d372fa03bc2ef2674ac1
Johannes Egger <j.egger@posteo.at>
2019-06-15 14:18:05 +0200

    Increase timeout for pushing the NuGet package

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -3,4 +3,4 @@ Param(
   [Parameter(Mandatory=$True)][string]$apiKey
 )
 
-dotnet nuget push $packagePath -s https://api.nuget.org/v3/index.json -k $apiKey
+dotnet nuget push $packagePath -s https://api.nuget.org/v3/index.json -k $apiKey --timeout 500
