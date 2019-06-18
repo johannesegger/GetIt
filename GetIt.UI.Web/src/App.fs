7065bbd409cfe0a72551642ce2f895628a2e5447
Johannes Egger <j.egger@posteo.at>
2019-06-18 10:15:14 +0200

    Remove unused parts from fable-elmish-react template

--- GetIt.UI.Web/src/App.fs
+++ GetIt.UI.Web/src/App.fs
@@ -1,72 +1,28 @@
 module App.View
 
 open Elmish
-open Elmish.Browser.Navigation
-open Elmish.Browser.UrlParser
-open Fable.Core
+open Elmish.Debug
+open Elmish.React
+open Elmish.HMR // Must be last Elmish.* open declaration (see https://elmish.github.io/hmr/#Usage)
 open Fable.Core.JsInterop
-open Fable.Import
-open Fable.Import.Browser
-open Types
-open App.State
-open Global
+open Fable.Helpers.React
 
 importAll "../sass/main.sass"
 
-open Fable.Helpers.React
-open Fable.Helpers.React.Props
+type Msg = class end
 
-let menuItem label page currentPage =
-    li
-      [ ]
-      [ a
-          [ classList [ "is-active", page = currentPage ]
-            Href (toHash page) ]
-          [ str label ] ]
+type Model = unit
 
-let menu currentPage =
-  aside
-    [ ClassName "menu" ]
-    [ p
-        [ ClassName "menu-label" ]
-        [ str "General" ]
-      ul
-        [ ClassName "menu-list" ]
-        [ menuItem "Home" Home currentPage
-          menuItem "Counter sample" Counter currentPage
-          menuItem "About" Page.About currentPage ] ]
+let init () = (), Cmd.none
 
-let root model dispatch =
+let update msg model =
+    model, Cmd.none
 
-  let pageHtml page =
-    match page with
-    | Page.About -> Info.View.root
-    | Counter -> Counter.View.root model.Counter (CounterMsg >> dispatch)
-    | Home -> Home.View.root model.Home (HomeMsg >> dispatch)
-
-  div
-    []
-    [ Navbar.View.root
-      div
-        [ ClassName "section" ]
-        [ div
-            [ ClassName "container" ]
-            [ div
-                [ ClassName "columns" ]
-                [ div
-                    [ ClassName "column is-3" ]
-                    [ menu model.CurrentPage ]
-                  div
-                    [ ClassName "column" ]
-                    [ pageHtml model.CurrentPage ] ] ] ] ]
-
-open Elmish.React
-open Elmish.Debug
-open Elmish.HMR
+let view model dispatch =
+  div [] []
 
 // App
-Program.mkProgram init update root
-|> Program.toNavigable (parseHash pageParser) urlUpdate
+Program.mkProgram init update view
 #if DEBUG
 |> Program.withDebugger
 #endif
