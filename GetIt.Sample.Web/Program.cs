163e7d18d4b9f8718944310578bd2e37acc0ac41
Johannes Egger <j.egger@posteo.at>
2019-06-18 09:52:49 +0200

    Add sample project

---
+++ GetIt.Sample.Web/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GetIt;
+
+namespace GetIt.Sample.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.ShowScene();
+            // Game.ShowScene(300, 200);
+            // Game.ShowMaximizedScene();
+        }
+    }
+}
