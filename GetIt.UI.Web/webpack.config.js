c51d3ce6749c2b4745fcc15dbc44e35f65a74fa4
Johannes Egger <j.egger@posteo.at>
2019-06-18 09:04:25 +0200

    Set dev server proxy in webpack.config.js

--- GetIt.UI.Web/webpack.config.js
+++ GetIt.UI.Web/webpack.config.js
@@ -23,7 +23,16 @@ var CONFIG = {
     devServerPort: 8080,
     // When using webpack-dev-server, you may need to redirect some calls
     // to a external API server. See https://webpack.js.org/configuration/dev-server/#devserver-proxy
-    devServerProxy: undefined,
+    devServerProxy: {
+        "/api/*": {
+            target: "http://localhost:1503",
+            changeOrigin: true
+        },
+        "/socket": {
+            target: "http://localhost:1503",
+            ws: true
+        }
+    },
     // Use babel-preset-env to generate JS compatible with most-used browsers.
     // More info at https://babeljs.io/docs/en/next/babel-preset-env.html
     babel: {
