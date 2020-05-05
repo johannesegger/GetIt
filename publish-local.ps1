abacd6767692deda408afef2228f3429d5b0112a
johannesegger <j.egger@posteo.at>
2020-05-05 22:14:19 +0200

    Add script to create and publish NuGet package locally

---
+++ publish-local.ps1
@@ -0,0 +1,9 @@
+﻿$version = "3.1.4"
+dotnet build -c Release .\GetIt.Controller\
+dotnet build -c Release .\GetIt.UI.Container\
+# Push-Location .\GetIt.UI
+# yarn webpack
+# Pop-Location
+.\.paket\paket.exe pack --minimum-from-lock-file --version $version GetIt.Controller
+Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\
+dotnet add $project package GetIt -s "$pwd\nuget" -v $version
