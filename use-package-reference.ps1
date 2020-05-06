e3ad119058d4951ea7ebaf988141ae4215be8d5c
johannesegger <j.egger@posteo.at>
2020-05-06 10:57:30 +0200

    Improve script to switch to package reference

--- use-package-reference.ps1
+++ use-package-reference.ps1
@@ -4,4 +4,5 @@ Param(
 )
 
 dotnet remove $project reference ..\GetIt.Controller\GetIt.Controller.fsproj
-dotnet add $project package GetIt -s https://pkgs.dev.azure.com/eggerhansi/_packaging/GetIt@Local/nuget/v3/index.json -v $version
+# TODO install https://github.com/microsoft/artifacts-credprovider
+dotnet add $project package GetIt -s https://pkgs.dev.azure.com/eggerhansi/_packaging/GetIt@Local/nuget/v3/index.json --interactive -v $version
