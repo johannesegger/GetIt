ec02f20b8eb0a6f92a216503e286c18795fbb3aa
Johannes Egger <j.egger@posteo.at>
2020-11-21 07:57:04 +0100

    Rename fix-nupkg.ps1

--- publish-local.ps1
+++ publish-local.ps1
@@ -3,5 +3,5 @@ dotnet publish -r win10-x86 -c Release --self-contained false -p:PublishSingleFi
 .\.paket\paket.exe pack --minimum-from-lock-file --version $version .\GetIt.Controller
 mkdir .\nuget -Force | Out-Null
 Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\ -Force
-.\fix-nupkg.ps1 .\nuget\GetIt.$version.nupkg
+.\add-aspnetcore-reference.ps1 .\nuget\GetIt.$version.nupkg
 dotnet add .\GetIt.Sample package GetIt -s "$pwd\nuget" -v $version
