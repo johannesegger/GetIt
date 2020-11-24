67f79b96bd8159794e3210a90491d7b08396e482
Johannes Egger <j.egger@posteo.at>
2020-11-24 21:32:20 +0100

    Don't add package reference when publishing locally

--- publish-local.ps1
+++ publish-local.ps1
@@ -4,4 +4,3 @@ dotnet publish -r win10-x86 -c Release --self-contained false -p:PublishSingleFi
 mkdir .\nuget -Force | Out-Null
 Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\ -Force
 .\add-aspnetcore-reference.ps1 .\nuget\GetIt.$version.nupkg
-dotnet add .\GetIt.Sample package GetIt -s "$pwd\nuget" -v $version
