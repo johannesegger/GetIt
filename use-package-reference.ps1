ec3f7fc7917a77e0896c07340a5db438e9b8a72e
Johannes Egger <j.egger@posteo.at>
2020-11-24 21:32:45 +0100

    Use local directory when adding package reference

--- use-package-reference.ps1
+++ use-package-reference.ps1
@@ -4,5 +4,4 @@ Param(
 )
 
 dotnet remove $project reference ..\GetIt.Controller\GetIt.Controller.fsproj
-# TODO install https://github.com/microsoft/artifacts-credprovider
-dotnet add $project package GetIt -s https://pkgs.dev.azure.com/eggerhansi/_packaging/GetIt@Local/nuget/v3/index.json --interactive -v $version
+dotnet add $project package GetIt -s $PSScriptRoot\nuget -v $version
