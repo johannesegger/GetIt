ecc00cb6d0aa32f10be465aa3960ff58e81d3dcd
johannesegger <j.egger@posteo.at>
2022-01-29 06:58:11 +0100

    Build UI with x64

--- publish-local.ps1
+++ publish-local.ps1
@@ -1,7 +1,8 @@
-﻿$version = "3.2.4-alpha0008"
-dotnet publish -r win10-x86 -c Release --self-contained false -p:PublishSingleFile=True -p:DebugType=None .\GetIt.UI.Container
-dotnet publish .\GetIt.Controller
-.\.paket\paket.exe pack --build-config Debug --minimum-from-lock-file --version $version .\GetIt.Controller
+﻿$version = "3.2.6-alpha0005"
+dotnet clean
+dotnet publish -r win10-x64 -c Release --no-self-contained -p:PublishSingleFile=True -p:DebugType=None .\GetIt.UI.Container
+dotnet build -c Release .\GetIt.Controller
+.paket\paket.exe pack --build-config Release --minimum-from-lock-file --version $version .\GetIt.Controller
 mkdir .\nuget -Force | Out-Null
 Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\ -Force
 .\add-aspnetcore-reference.ps1 .\nuget\GetIt.$version.nupkg
