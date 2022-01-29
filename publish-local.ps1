02b12099b92a8b9e88c672d64e861272e6929ff8
johannesegger <j.egger@posteo.at>
2022-01-29 07:08:27 +0100

    Don't publish UI as single file

--- publish-local.ps1
+++ publish-local.ps1
@@ -1,6 +1,5 @@
-﻿$version = "3.2.6-alpha0005"
-dotnet clean
-dotnet publish -r win10-x64 -c Release --no-self-contained -p:PublishSingleFile=True -p:DebugType=None .\GetIt.UI.Container
+﻿$version = "3.2.6-alpha0007"
+dotnet publish -r win10-x64 -c Release --no-self-contained -p:DebugType=None .\GetIt.UI.Container
 dotnet build -c Release .\GetIt.Controller
 .paket\paket.exe pack --build-config Release --minimum-from-lock-file --version $version .\GetIt.Controller
 mkdir .\nuget -Force | Out-Null
