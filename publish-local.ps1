feb33c196bdc38c78bbc0ae1e1985633f329b0bc
Johannes Egger <j.egger@posteo.at>
2023-05-20 07:51:17 +0200

    Fix publish-local.ps1

--- publish-local.ps1
+++ publish-local.ps1
@@ -1,4 +1,5 @@
 ﻿$version = "3.2.6-alpha0007"
+dotnet build -c Release .\GetIt.Controller
 .paket\paket.exe pack --build-config Release --minimum-from-lock-file --version $version .\GetIt.Controller
 mkdir .\nuget -Force | Out-Null
 Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\ -Force
