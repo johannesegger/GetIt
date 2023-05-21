1ae30161b618ff07b2803312ab4f39415980641b
Johannes Egger <j.egger@posteo.at>
2023-05-21 07:44:49 +0200

    Switch from Paket back to NuGet
    Both have their weirdnesses, Paket seems somewhat stale at the moment

--- publish-local.ps1
+++ publish-local.ps1
@@ -1,6 +1,2 @@
-﻿$version = "3.2.6-alpha0007"
-dotnet build -c Release .\GetIt.Controller
-.paket\paket.exe pack --build-config Release --minimum-from-lock-file --version $version .\GetIt.Controller
-mkdir .\nuget -Force | Out-Null
-Move-Item .\GetIt.Controller\GetIt.$version.nupkg .\nuget\ -Force
-.\fix-nupkg.ps1 .\nuget\GetIt.$version.nupkg
+﻿$version = "3.3.0-alpha0004"
+dotnet pack --configuration Release --output .\nuget -p:PackageVersion=$version .\GetIt.Controller
